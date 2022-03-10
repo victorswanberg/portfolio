f11a6c0741cef61058c8e257515a2c9834bef25c
Victor Swanberg <victor.swanberg@sublimemedia.com>
2022-03-09 18:49:28 -0800

    Added in more navbar code and a logo icon.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo-white.png';
+//React Font Awesome imports
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons'
+
+
 const Navbar = () => {
  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="pushing limits connecting people" /></a>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container">
+      <a className="navbar-brand" href="#">
+        <img className="logo" src={logo} alt="pushing limits connecting people" />
+      </a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+        {/* Need to add the font awesome component */}
+        <FontAwesomeIcon icon={faBars} style={{color:"#fff"}}/>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
+        <ul className="navbar-nav ml-auto">
           <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="#">Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
+            <a className="nav-link" href="#">About me</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Portfolio</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Services</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Contact</a>
           </li>
         </ul>
       </div>

98570c714d7d0e751954e624bf95c4a0f09a6df4
Victor Swanberg <victor.swanberg@sublimemedia.com>
2022-03-14 08:40:02 -0700

    Added more custom styles for about me in mobile mode

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,7 +12,7 @@ const AboutMe = () => {
                     </div>
                 </div>
                 <div className="col-lg-6 col-xm-12">
-                    <h1>About me</h1>
+                    <h1 className="profile-header mb-1">About me</h1>
                     <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>

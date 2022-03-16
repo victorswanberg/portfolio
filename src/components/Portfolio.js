86822f7d1319e76ac4713a925be06262a9247f87
Victor Swanberg <victor.swanberg@sublimemedia.com>
2022-03-16 08:08:30 -0700

    added a new portfolio component

---
+++ src/components/Portfolio.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return(
+        <div className="portfolio-wrapper">
+            <div className="container">
+                <h1 className="text-uppercase text-center py-5">
+                    Portfolio
+                    <div className="image-box-wrapper row justify-content-center">
+                        <img className="portfolio-image" alt="" />
+                    </div>
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Portfolio; 

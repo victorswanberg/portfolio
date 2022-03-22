e37269d8fbd9256955cb1e1ccc949b0ef7292f5e
Victor Swanberg <victor.swanberg@sublimemedia.com>
2022-03-22 12:17:04 -0700

    Added in some transition effects on focus

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -12,30 +12,59 @@ const Contacts = () => {
       <div className="container">
         <div className="row">
           <div className="col-md-6 col-xs-12">
-            {/* Name input */}
+          <div className="line"></div>
+            {/* Description input */}
             <input
+            id="name"
             className="form-control"
             placeholder="Name"
             name="name"
+            type="text"
             />
-             {/* Phone input */}
-             <input
+            <div className="line"></div>
+            {/* Phone input */}
+            <input
+            id="phone"
             className="form-control"
             placeholder="Phone"
             name="phone"
+            type="text"
             />
+            <div className="line"></div>
              {/* Email input */}
-             <input
+            <input
+            id="email"
             className="form-control"
             placeholder="Email"
             name="email"
+            type="email"
             />
+            <div className="line"></div>
              {/* Subject input */}
-             <input
+            <input
+            id="subject"
             className="form-control"
             placeholder="Subject"
             name="subject"
+            type="text"
             />
+          </div>
+          <div className="col-md-6 col-xs-12">
+          <div className="line"></div>
+            {/* Description text */}
+            <textarea 
+            id="descruption"
+            className="form-control"
+            type="text"
+            placeholder="Description"
+            />
+            <button className="btn-main-offer contact-btn" 
+            type="submit">
+              Contact Me
+            </button>
+           
+            
+            
           </div>
         </div>
       </div>
